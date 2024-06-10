c14d0553c44540a2f75e1d51f022685868b5da6e
Avngrss <sedow94@mail.ru>
2024-06-10 19:06:22 +0300

    add packages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,5 +4,16 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+  ],
+};
+const firebaseConfig = {
+  apiKey: 'AIzaSyCf9PMjercuhwXM3eatGsk9YnL_IlJGNI8',
+  authDomain: 'angular-shop-831f1.firebaseapp.com',
+  projectId: 'angular-shop-831f1',
+  storageBucket: 'angular-shop-831f1.appspot.com',
+  messagingSenderId: '125261700241',
+  appId: '1:125261700241:web:3030cf47152907f9783330',
 };
