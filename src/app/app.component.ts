cf4f924b1c3c0b6a2a183762ee90e054155cdded
Avngrss <sedow94@mail.ru>
2024-06-13 00:20:43 +0300

    add component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,23 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ApiserviseService } from './services/apiservise.service';
 import { Flower } from './types/flower.interface';
+import { CardComponent } from './components/card/card.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  imports: [
+    RouterOutlet,
+    CardComponent,
+    SliderComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
 })
 export class AppComponent {
   flowerService = inject(ApiserviseService);
